6a34b3e800061b1531cd9d887db6a2d6b0f0bcb2
Diyanshi <2k22.cscys.2212176@gmail.com>
2026-05-01 23:57:28 +0530

    fix: remove trailing slash everywhere

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://team-task-manager-production-a40e.up.railway.app/" // 👈 replace this
+  baseURL: "https://team-task-manager-production-a40e.up.railway.app" // 👈 replace this
 });
 
 export default api;
