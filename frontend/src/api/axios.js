bce41b3e97cb160b1d4ea36f31c2226161c7931a
Diyanshi <2k22.cscys.2212176@gmail.com>
2026-05-01 23:45:03 +0530

    connect frontend to backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,17 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+  baseURL: "https://team-task-manager-production-a40e.up.railway.app/" // 👈 replace this
 });
 
 export default api;
